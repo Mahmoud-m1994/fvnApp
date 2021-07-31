e0fd5b9773254ddfcee00166f22b9ea348b8046b
Mahmoud Ibrahim <mahmouim@stud.ntnu.no>
2021-07-31 02:34:57 +0200

    FVN-7 created a dynamic page with dynamic url based on post id

---
+++ sommervennen/pages/_id.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+      <h1>This is dynamic page</h1>
+      <h2>{{filteredPost.title}}</h2>
+    </div>
+</template>
+
+
+<script>
+export default {
+  async asyncData({ params, redirect }) {
+    const posts = await fetch(
+      'https://jsonplaceholder.typicode.com/posts'
+    ).then((res) => res.json())
+
+    const filteredPost = posts.filter(
+      (e) => e.id === params.id
+    )
+    if (filteredPost) {
+      return {
+        filteredPost
+      }
+    } else {
+      console.log(filteredPost[0]);
+      console.log("Mahi ", params.id);
+      redirect('/')
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
